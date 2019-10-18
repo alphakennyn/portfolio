78006811fb9b81a55c43709e4473e67bb2b696f6
kenny nguyen <kenny.nguyen@dynamicly.com>
2019-10-18 18:42:09 -0400

    ✨ add projects page

--- src/components/page2/index.js
+++ src/components/page2/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
-// import './styles.scss';
+import projects from '../../data/projects.json';
 
-const NameComponent = (props) => {
-    return (
-        <div className="name-container">
-            hello
-        </div>
-      );
-}
+import './styles.scss';
+
+/**
+ * @description project cards
+ * @param {*} props
+ */
+const ProjectCard = ({ title, description, link }) => {
+  return (
+    <div className="project-container">
+      <div className="project_header">
+        <h2 className="project_title">{title}</h2>
+      </div>
+      <div className="project_body">
+        <div className="project_description">{description}</div>
+      </div>
+      <div className="project_footer">
+        <a className="project_link" href={link}>Play now</a>
+      </div>
+    </div>
+  );
+};
 
 
 const Page = (props) => {
+  const cards = projects.map(({ id, ...rest }) => <ProjectCard {...rest} key={id}/>)
   return (
-    <div className="page">
-        <NameComponent />
+    <div className="page page_projects">
+      {cards}
     </div>
   );
 }
