4511297da1d9b771a3cc51ed29506466953a6771
Kenny Nguyen <alphakennyn@gmail.com>
2019-10-24 17:06:01 -0400

    Updates

---
+++ precache-manifest.721216d87ff690f2cd14bf3b75cb2694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f699527350df71fc2e2f34c4602d55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8609524e3ecc5b7c2f4b",
+    "url": "/static/css/main.9c37288d.chunk.css"
+  },
+  {
+    "revision": "b3d730ad145837530b12",
+    "url": "/static/js/2.89a1fd70.chunk.js"
+  },
+  {
+    "revision": "8609524e3ecc5b7c2f4b",
+    "url": "/static/js/main.1c9d5171.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "b0fcac5b90b0a7cb94d3cd3ef3eae12e",
+    "url": "/static/media/lego-contruction.b0fcac5b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8ad74b7d4aa080560f6754390c01c178.js"
+  "/precache-manifest.721216d87ff690f2cd14bf3b75cb2694.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
