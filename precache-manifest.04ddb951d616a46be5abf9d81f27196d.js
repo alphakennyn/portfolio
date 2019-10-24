01c8de4f2130f2418ae1e4bd9a95b2b7a895d679
Kenny Nguyen <alphakennyn@gmail.com>
2019-10-24 00:47:40 -0400

    Updates

---
+++ precache-manifest.04ddb951d616a46be5abf9d81f27196d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821d938464d46b12af59661cad97a4f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcaf91f5c48fbc0a44ae",
+    "url": "/static/css/main.7177f5a3.chunk.css"
+  },
+  {
+    "revision": "5cc78e92e7596a72583e",
+    "url": "/static/js/2.eac80404.chunk.js"
+  },
+  {
+    "revision": "bcaf91f5c48fbc0a44ae",
+    "url": "/static/js/main.88614780.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  }
+]);
