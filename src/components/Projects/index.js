89af22cfb66886558c3352805c399b7d3a29a9cf
Kenny Nguyen <kenny.nguyen@makegoodfood.ca>
2020-10-24 17:31:25 -0400

    first commit node

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { DefaultCard } from '../misc/Card';
+import { DefaultCard, NodeCard } from '../misc/Card';
 
 import projects from '../../data/projects.json';
 
 import './styles.scss';
 
-
 const Page = (props) => {
-  const cards = projects.map(({ id, ...rest }) => <DefaultCard {...rest} key={id}/>)
+  const cards = projects.map(({ id, ...rest }) => <NodeCard {...rest} key={id}/>)
   return (
     <div className="page page_projects">
-      {cards}
+      <svg height="1000" width="1000">
+        {cards}
+      </svg>
     </div>
   );
 }
--- src/components/misc/Card/index.js
+++ src/components/misc/Card/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import {useTransition, animated} from 'react-spring'
+import throttle from 'lodash.throttle';
 import './styles.scss';
 
+import { hoverMultiplier } from '../../../data/constants.json';
 
 /**
  * @description project cards
@@ -40,4 +42,68 @@ export const CloudCard = ({ title, description, link, startTime, endTime }) => {
           </div>
         </div>
       );
+}
+
+const randomRange = (max = 400, min = -400) => {
+  return Math.random() * (max - min) + min;
+}
+
+export const NodeCard = ({ title, radiusSize = 50 }) => {
+  const [hover, setHover] = useState(false);
+  const [xPosition, setXPosition] = useState(randomRange());
+  const [yPosition, setYPosition] = useState(randomRange());
+
+
+  const name = title.slice(0, 1).toUpperCase();
+
+  const [items, setItems] = useState([name])
+  const transitions = useTransition(items, null, {
+    from: { transform: 'translate3d(0,-40px,0)', opacity: 1 },
+    enter: { transform: 'translate3d(0,0,0)', opacity: 1, delay: 300 },
+    leave: { transform: 'translate3d(0,40px,0)', opacity: 0 },
+  })
+
+  // return transitions.map(({ item, props, key }) =>
+  //   <animated.div key={key} style={props}>{item.text}</animated.div>
+  // )
+  const hoverEvent = () => {
+    setHover(true)
+    setItems([...title]);
+    console.log(items)
+  }
+  const hoverLeaveEvent = () => {
+    setHover(false)
+    setItems([name]);
+    console.log(items)
+  }
+  
+  // const name = title.slice(0, 1).toUpperCase();
+  const realRadius = hover ? radiusSize * hoverMultiplier : radiusSize;
+  const name1 = transitions.map(({ item, props, key }) => <animated.div key={key} style={props}>{item.text}</animated.div>);
+  return (
+    <g 
+      className="node-card"
+      onMouseEnter={hoverEvent}
+      onMouseLeave={hoverLeaveEvent}
+    >
+      <circle className="node-card" cx="500" cy="500" r={realRadius}>
+      </circle>
+      {
+        transitions.map(({ item, props, key }, index) => 
+          <animated.text
+          // style={"position: relative"}
+          x="50%" 
+          y="50%" 
+          // dx={`${index}%`}
+          text-anchor="middle" 
+          stroke="#51c5cf" 
+          key={key} 
+            style={props}
+          >{item}</animated.text>)
+      }
+      {/* <text x="50%" y="50%" text-anchor="middle" stroke="#51c5cf" stroke-width="2px">
+        { name }
+      </text> */}
+    </g>
+  )
 }
